bf5d320f69ab335bed1c6c521bb42547026eeed8
Mallory Howard <mallory1313@msn.com>
2024-02-08 18:46:19 -0700

    fixed modal. combined js, html, and css sheets

--- Script/next-page.js
+++ Script/next-page.js
@@ -1,3 +1,8 @@
+// Add function to ensure DOM is fully loaded
+
+// fetchAndDisplayRandomJokes
+// addJokeToLocalStorage
+
 let jokeList = document.getElementById('joke-list');
 let jokes = JSON.parse(localStorage.getItem('dadJokes'));
 let favorite = [];
@@ -8,43 +13,73 @@ console.log(jokes);
 const ratingStars = [...document.getElementsByClassName(".rating__star")];
 
 for (let i = 0; i < jokes.length; i++) {
-  let li = document.createElement('li')
-  li.setAttribute('joke', jokes[i].content)
-  li.innerHTML = `${jokes[i].date} <br> ${jokes[i].content}`;
+  let li = document.createElement('li');
+  li.setAttribute('joke', jokes[i].content);
+  // li.innerHTML = `${jokes[i].date} <br> ${jokes[i].content}`;
 
-  let b = document.createElement('button');
-  b.innerText = 'Add to Favorites';
-  b.setAttribute('id', `fav-btn-${i}`);
-  b.addEventListener('click', () => 
-  addFavorite(i))
+  let jokeDate = document.createElement('h6');
+  jokeDate.textContent = jokes[i].date;
+  li.append(jokeDate);
 
-  li.appendChild(b);
+  let jokeText = document.createElement('p');
+  jokeText.textContent = jokes[i].content;
+  li.append(jokeText);
 
-    // Create a div for the star rating and add it to the li
-    let starRatingDiv = document.createElement('div');
-    starRatingDiv.classList.add('rating');
 
-   // Create a new set of five stars for each li
-   for (let j = 0; j < 5; j++) {
+  // Dynamically create favorite buttons
+  let buttonFav = document.createElement('button');
+  buttonFav.innerText = 'Add to Favorites';
+  buttonFav.setAttribute('id', `fav-btn-${i}`);
+  buttonFav.addEventListener('click', () =>
+    addFavorite(i))
+
+  // li.appendChild(buttonFav);
+
+  // Dynamically create send email buttons
+  let buttonEmail = document.createElement('button');
+  buttonEmail.innerText = 'Send Joke as Email';
+  buttonEmail.setAttribute('id', `email-btn-${i}`);
+  buttonEmail.setAttribute('class', 'email-btn js-modal-trigger');
+  buttonEmail.setAttribute('data-target', 'modal-js-example');
+
+  // Wrap buttons in a div for styling
+  let buttonsDiv = document.createElement('div');
+  buttonsDiv.style.display = 'flex'; // Use Flexbox
+  buttonsDiv.style.gap = '10px'; // Optional: adds space between buttons
+
+  buttonsDiv.appendChild(buttonFav);
+  buttonsDiv.appendChild(buttonEmail);
+
+  // Append the div containing both buttons to the li
+  li.appendChild(buttonsDiv);
+
+
+  // Create a div for the star rating and add it to the li
+  let starRatingDiv = document.createElement('div');
+  starRatingDiv.classList.add('rating');
+
+  // Create a new set of five stars for each li
+  for (let j = 0; j < 5; j++) {
     let star = document.createElement('i');
     star.classList.add('rating__star', 'fa-regular', 'fa-star');
     starRatingDiv.appendChild(star);
   }
 
-    li.appendChild(starRatingDiv);
-    
+  li.appendChild(starRatingDiv);
 
   jokeList.appendChild(li);
 
-    // Execute the rating function for each li element
-    const ratingStarsInLi = [...li.querySelectorAll('.rating .rating__star')];
-    executeRating(ratingStarsInLi, i); // Pass the joke index as a parameter
+  // Execute the rating function for each li element
+  const ratingStarsInLi = [...li.querySelectorAll('.rating .rating__star')];
+  executeRating(ratingStarsInLi, i); // Pass the joke index as a parameter
 
   // Set the initial state of stars based on the stored rating
   const storedRating = jokes[i].rating || 0;
   setInitialStarsState(ratingStarsInLi, storedRating);
-}
+} // end of for (let i = 0; i < jokes.length; i++)
 
+
+// Beginning: Favorite items section
 let favoriteList = document.getElementById('favorite-list');
 if (!favoriteList) {
   favoriteList = document.createElement('ul');
@@ -54,12 +89,12 @@ if (!favoriteList) {
 
 function addFavorite(jokeIndex) {
   const selectedJoke = jokes[jokeIndex];
-  
+
   // Check if the joke is not already in the favorite array
   if (!favorite.includes(selectedJoke)) {
     favorite.push(selectedJoke);
     console.log(`Joke added to favorites: ${selectedJoke.content}`);
-  
+
     // Optionally, you can update the UI to reflect that the joke is now a favorite
     // For example, you can change the button text or color
     updateFavoriteButton(jokeIndex, true);
@@ -111,7 +146,7 @@ function updateFavoriteButton(jokeIndex, Favorite) {
     favBtn.innerText = Favorite ? 'Remove from Favorites' : 'Add to Favorites';
     favBtn.classList.toggle('fave-btn-remove', Favorite);
     // Optionally, you can also change the button color or style
-  } 
+  }
 }
 
 // Optionally, you can also initialize the UI based on the existing favorites
@@ -125,9 +160,10 @@ for (let i = 0; i < jokes.length; i++) {
 }
 
 displayFavoriteJokes();
+// End: Favorite items section
 
-// code for star rating
 
+// Beginning: code for star rating
 function setInitialStarsState(stars, rating) {
   const starClassActive = "fa-solid";
   const starClassInactive = "fa-regular";
@@ -146,7 +182,7 @@ function executeRating(stars, jokeIndex) {
   const starClassActive = "fa-solid";
   const starClassInactive = "fa-regular";
   const starsLength = stars.length;
- 
+
   stars.map((star, index) => {
     star.onclick = () => {
       for (let i = 0; i <= index; i++) {
@@ -184,3 +220,148 @@ document.querySelectorAll('#joke-list li').forEach((li, index) => {
   const ratingStarsInLi = [...li.querySelectorAll('.rating .rating__star')];
   executeRating(ratingStarsInLi, index);
 });
+// End: code for star rating
+
+
+
+// When "Send Joke as Email" is clicked, capture the text value from the p tag that triggered event and save for sendEmail function
+let globalJokeTextValue = ''; 
+
+const buttons = document.querySelectorAll('.email-btn');
+buttons.forEach(button => {
+  button.addEventListener('click', function (event) {
+    // This function is called when a button is clicked.
+    // Additional logic to ensure it's an email-btn
+    if (event.target.classList.contains('email-btn')) {
+      const jokeTextP = event.target.closest('li').querySelector('p'); // Selecting the <p> element directly now
+      globalJokeTextValue = jokeTextP ? jokeTextP.textContent : 'Joke not found'; // Use textContent to get just the text
+
+      console.log('Joke text:', globalJokeTextValue);
+    }
+  });
+});
+
+
+// When form is submitted, get jokeTextValue and initiate sendEmail function
+var form = document.getElementById('emailForm');
+
+// Beginning: When "Send Email" is clicked, get form values, initiate isValidEmail function, and determine when to send email
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Get form values
+    var email = document.getElementById('email').value;
+    var time = document.getElementById('time').value;
+
+    // Validate email format
+    if (!isValidEmail(email)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+    console.log("time is " + time);
+
+    // Convert "Scheduled Time" user input to milliseconds
+    var scheduledTime = new Date(time).getTime();
+
+    // Get current time and round it down to the nearest minute (aka omit the seconds) for easier comparison
+    var now = new Date();
+    var currentTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes()).getTime();
+
+    console.log(currentTime)
+    console.log(scheduledTime)
+
+
+    // Check if the scheduled time is in the past, if so, present alert
+    if (scheduledTime < currentTime) {
+        alert('Please select a current or future time for sending the email.');
+        return;
+    }
+
+    // Calculate the delay until the scheduled time
+    var delay = scheduledTime - currentTime;
+
+    console.log("Delay " + delay);
+
+    // Schedule email sending after the specified delay
+    setTimeout(function () {
+        // Fetch a random dad joke from the icanhazdadjoke API and send email
+        sendEmail(email, globalJokeTextValue);
+    }, delay);
+
+    console.log('Email will be sent at:', new Date(scheduledTime));
+});
+// End: When "Send Email" is clicked, get form values, initiate isValidEmail function, and determine when to send email
+
+
+// Beginning: Ensure a valid email address is entered
+function isValidEmail(email) {
+    // Very basic email validation
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
+// End: Ensure a valid email address is entered
+
+// Beginning: Send Email
+emailjs.init('e4IMeDjiTLqb2QzFb'); //Your EmailJS user ID
+
+function sendEmail(email, joke) {
+  // Use EmailJS to send email
+  emailjs.send('service_wv4ctg4', 'template_036nw4v', {
+      to_email: email,
+      joke_content: joke
+  })
+      .then(function (response) {
+          console.log('Email sent:', response);
+          alert('Email sent successfully!');
+      }, function (error) {
+          console.error('Error sending email:', error);
+          alert('Failed to send email. Please try again later.');
+      });
+};
+// End: Send Email
+
+
+// Beginning: Modal functionality
+// Functions to open and close a modal
+function openModal($el) {
+  $el.classList.add('is-active');
+}
+
+function closeModal($el) {
+  $el.classList.remove('is-active');
+}
+
+// function closeAllModals() {
+//   (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+//     closeModal($modal);
+//   });
+// }
+
+// Add a click event on buttons to open a specific modal
+(document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+  const modal = $trigger.dataset.target;
+  const $target = document.getElementById(modal);
+
+  $trigger.addEventListener('click', () => {
+    openModal($target);
+  });
+});
+
+// Add a click event on various child elements to close the parent modal
+(document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+  const $target = $close.closest('.modal');
+
+  $close.addEventListener('click', () => {
+    closeModal($target);
+  });
+});
+
+// // Add a keyboard event to close all modals
+// document.addEventListener('keydown', (event) => {
+//   if(e.key === "Escape") {
+//     closeAllModals();
+//   }
+// });
+
+// End: Modal functionality
